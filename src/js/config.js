c238754d8518914b3f379c603609e75dedbc9ba5
ereklemanjavidze <ereklemanjavidze@gmail.com>
2023-03-26 15:50:50 +0400

    Initialcommit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9d817e29-40c9-4496-b4bc-04fb1622dc40';
+export const MODAL_CLOSE_SEC = 2.5;
